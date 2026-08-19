01183f8c84f2b0cace41e56102386866f611580d
hoangtritoanpro-eng <hoangtritoanpro@gmail.com>
2026-08-19 13:48:27 +0700

    Fix api import in Login.jsx causing React crash

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { api } from '../api';
 import PublicNotices from '../components/public/PublicNotices';
 import PublicLibrary from '../components/public/PublicLibrary';
 import PublicClassInfo from '../components/public/PublicClassInfo';
